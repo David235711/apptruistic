9a46cf1ca0fbf26763586824f2e53bd01fcbb6e7
GinaMiso <eugenia.buccellato@gmail.com>
2020-05-25 13:56:16 +0200

    first push

---
+++ src/main/java/com/project/apptruistic/ApptruisticApplication.java
@@ -0,0 +1,13 @@
+package com.project.apptruistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptruisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptruisticApplication.class, args);
+	}
+
+}
