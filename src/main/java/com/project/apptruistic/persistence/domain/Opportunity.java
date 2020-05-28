90fd72fb84acf10fb01c30427eba004c12be9cf5
GinaMiso <eugenia.buccellato@gmail.com>
2020-05-28 17:20:09 +0200

    OpportunityEndpointTest POST does not pass

--- src/main/java/com/project/apptruistic/persistence/domain/Opportunity.java
+++ src/main/java/com/project/apptruistic/persistence/domain/Opportunity.java
@@ -1,14 +1,14 @@
 package com.project.apptruistic.persistence.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Objects;
 
-
 public class Opportunity {
+
     private String id;
     @NotEmpty
     private String name;
@@ -26,84 +26,93 @@ public class Opportunity {
     @NotEmpty
     private String category;
 
-    private Creator creator;
+    private String creator;
+    private String creatorName;
 
     public Opportunity() {
     }
 
-    public Opportunity(@NotEmpty String name, @NotEmpty String description, @NotEmpty LocalDate occurDate, @NotEmpty LocalTime startTime,
-                       @NotEmpty LocalTime endTime, @NotEmpty String category, Creator creator) {
+    public Opportunity(@NotEmpty String name, @NotEmpty String description, @NotEmpty LocalDate occurDate, @NotEmpty LocalTime startTime, @NotEmpty LocalTime endTime, @NotEmpty String category, String creator, String creatorName) {
         this.name = name;
         this.description = description;
         this.occurDate = occurDate;
         this.startTime = startTime;
         this.endTime = endTime;
         this.category = category;
-        this.creator =creator;
+        this.creator = creator;
+        this.creatorName = creatorName;
     }
 
     public String getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public LocalDate getOccurDate() {
-        return occurDate;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public Creator getCreator() {
-        return creator;
-    }
-
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public LocalDate getOccurDate() {
+        return occurDate;
     }
 
     public void setOccurDate(LocalDate occurDate) {
         this.occurDate = occurDate;
     }
 
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
     public void setStartTime(LocalTime startTime) {
         this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
     }
 
     public void setEndTime(LocalTime endTime) {
         this.endTime = endTime;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setCreator(Creator creator) {
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
         this.creator = creator;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
     }
 
     @Override
@@ -118,12 +127,12 @@ public class Opportunity {
                 Objects.equals(startTime, that.startTime) &&
                 Objects.equals(endTime, that.endTime) &&
                 Objects.equals(category, that.category) &&
-                Objects.equals(creator, that.creator);
+                Objects.equals(creator, that.creator) &&
+                Objects.equals(creatorName, that.creatorName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, description, occurDate, startTime, endTime, category, creator);
+        return Objects.hash(id, name, description, occurDate, startTime, endTime, category, creator, creatorName);
     }
-
 }
--- src/test/java/com/project/apptruistic/communication/endpoint/OpportunityEndpointTest.java
+++ src/test/java/com/project/apptruistic/communication/endpoint/OpportunityEndpointTest.java
@@ -1,24 +1,18 @@
 package com.project.apptruistic.communication.endpoint;
 
 import com.project.apptruistic.logic.OpportunityService;
-import com.project.apptruistic.logic.VolunteerService;
-import com.project.apptruistic.persistence.domain.Individual;
 import com.project.apptruistic.persistence.domain.Opportunity;
-import com.project.apptruistic.persistence.domain.Volunteer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-/*
-import java.sql.Date;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
-import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -30,19 +24,32 @@ class OpportunityEndpointTest {
     @MockBean
     OpportunityService service;
 
-    @SpyBean
-    OpportunityEndpoint endpoint;
-
     String url = "/opportunities";
 
     @Test
-    void postIsValid() {
+    void post() {
         Opportunity opportunity = new Opportunity("help", "description", LocalDate.now(), LocalTime.now(), LocalTime.now(), "category",
-                List.of(new Individual()));
+                "individual", "creatorName");
+
         testRestTemplate.postForObject(url, opportunity, Opportunity.class);
+
+        verify(service).save(opportunity);
 
     }
 
-}
+    @Test
+    void getOne() {
+        String name = "help";
 
- */
+        testRestTemplate.getForObject(url + "/" + name, Opportunity.class);
+
+        verify(service).get(name);
+    }
+
+    @Test
+    void getAll() {
+        testRestTemplate.getForObject(url, Opportunity[].class);
+
+        verify(service).getAll();
+    }
+}
