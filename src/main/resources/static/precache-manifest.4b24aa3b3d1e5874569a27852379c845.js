f543defd366dbebaf3055bf7dc5e97c418431080
David235711 <info@davidwang.at>
2020-05-30 11:08:52 +0200

    added static React files

---
+++ src/main/resources/static/precache-manifest.4b24aa3b3d1e5874569a27852379c845.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83cc92946d036b4e8d9eafa272df1b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e31aacad7a0c24042cb1",
+    "url": "/static/css/main.5f5926ea.chunk.css"
+  },
+  {
+    "revision": "0a74a9e950d3f8a6a573",
+    "url": "/static/js/2.17b771b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.17b771b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e31aacad7a0c24042cb1",
+    "url": "/static/js/main.bcb10770.chunk.js"
+  },
+  {
+    "revision": "d8744125f3b318ef7338",
+    "url": "/static/js/runtime-main.38fcacda.js"
+  },
+  {
+    "revision": "2e64861a3b56665073796bfcfe88ae73",
+    "url": "/static/media/Team-Work.2e64861a.svg"
+  },
+  {
+    "revision": "c82937432a0762cbf483ccffaf8730da",
+    "url": "/static/media/logo.c8293743.svg"
+  }
+]);
