1a632d24bbf78d1b8ba76c1117660c6d8f9c5a53
David235711 <info@davidwang.at>
2020-06-07 23:24:11 +0200

    date fix in signuprequest, corsconfig opened

--- src/main/java/com/project/apptruistic/security/payload/request/SignupRequest.java
+++ src/main/java/com/project/apptruistic/security/payload/request/SignupRequest.java
@@ -22,7 +22,7 @@ public class SignupRequest {
     @NotBlank
     private String lastName;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
     @NotBlank
