eea33b814d8a946478b17b886c166983f1616ceb
GinaMiso <eugenia.buccellato@gmail.com>
2020-06-09 13:34:27 +0200

    opportunity class updated

--- src/main/java/com/project/apptruistic/logic/OpportunityService.java
+++ src/main/java/com/project/apptruistic/logic/OpportunityService.java
@@ -47,7 +47,6 @@ public class OpportunityService {
         return opportunityRepository.findAll();
     }
 
-
     public List<Opportunity> getAllAvailables() {
         return opportunityRepository.findAllByDoneFalse();
     }
