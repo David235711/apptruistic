7ed598edb4faf5ca806708abf6e4c9737e272c6f
David235711 <info@davidwang.at>
2020-06-11 10:35:32 +0200

    adding new static files

---
+++ src/main/resources/static/precache-manifest.1f0baa8d3cad5f0fbc298baf9bd28c14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ad6cc2136687f2b3b127e7a7ad82b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ea24139cb7fcaea3b9",
+    "url": "/static/css/main.c0f2d009.chunk.css"
+  },
+  {
+    "revision": "c0aad9224195a0092049",
+    "url": "/static/js/2.f34c24c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f34c24c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ea24139cb7fcaea3b9",
+    "url": "/static/js/main.3355c612.chunk.js"
+  },
+  {
+    "revision": "d8744125f3b318ef7338",
+    "url": "/static/js/runtime-main.38fcacda.js"
+  },
+  {
+    "revision": "2e64861a3b56665073796bfcfe88ae73",
+    "url": "/static/media/Team-Work.2e64861a.svg"
+  },
+  {
+    "revision": "c82937432a0762cbf483ccffaf8730da",
+    "url": "/static/media/logo.c8293743.svg"
+  }
+]);
