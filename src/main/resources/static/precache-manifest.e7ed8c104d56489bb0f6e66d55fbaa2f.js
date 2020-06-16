198fa5a0be876fbf09d2d75f6323f97dd8fb2d5f
David235711 <info@davidwang.at>
2020-06-16 16:37:50 +0200

    fresh static

---
+++ src/main/resources/static/precache-manifest.e7ed8c104d56489bb0f6e66d55fbaa2f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9594ddcf90f7c532f605bca29279f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac8afa681f726373cd11",
+    "url": "/static/css/main.1d16b018.chunk.css"
+  },
+  {
+    "revision": "d2b413e70f8f27615894",
+    "url": "/static/js/2.b3931c90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3931c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8afa681f726373cd11",
+    "url": "/static/js/main.bff88904.chunk.js"
+  },
+  {
+    "revision": "d8744125f3b318ef7338",
+    "url": "/static/js/runtime-main.38fcacda.js"
+  },
+  {
+    "revision": "2e64861a3b56665073796bfcfe88ae73",
+    "url": "/static/media/Team-Work.2e64861a.svg"
+  },
+  {
+    "revision": "aedbc5d5cdfd396dc6c68d6989db03d0",
+    "url": "/static/media/Team.aedbc5d5.svg"
+  },
+  {
+    "revision": "c82937432a0762cbf483ccffaf8730da",
+    "url": "/static/media/logo.c8293743.svg"
+  }
+]);
