c45a3e6722f5c86c0a2315e059deaa75c4b45f3f
David235711 <info@davidwang.at>
2020-06-17 23:49:33 +0200

    added boilerplate code for IndividualServiceTest and OrganizationServiceTest

---
+++ src/test/java/com/project/apptruistic/logic/IndividualServiceTest.java
@@ -0,0 +1,20 @@
+package com.project.apptruistic.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndividualServiceTest {
+
+    @MockBean
+    ApplicationRunner runner;
+
+    @Autowired
+    IndividualService individualService;
+
+}
