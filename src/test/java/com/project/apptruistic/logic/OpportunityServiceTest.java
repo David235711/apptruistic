ced612c868bf87ab5cc22e510a8c371d8c0221f4
David235711 <info@davidwang.at>
2020-06-17 23:41:46 +0200

    Unit tests working, OpportunityService and VolunteerService

--- src/test/java/com/project/apptruistic/logic/OpportunityServiceTest.java
+++ src/test/java/com/project/apptruistic/logic/OpportunityServiceTest.java
@@ -2,10 +2,9 @@ package com.project.apptruistic.logic;
 
 import com.project.apptruistic.persistence.domain.Opportunity;
 import com.project.apptruistic.persistence.repository.OpportunityRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -13,13 +12,16 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = NONE)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class OpportunityServiceTest {
+    @MockBean
+    ApplicationRunner applicationRunner;
 
     @Autowired
     OpportunityService opportunityService;
@@ -27,58 +29,55 @@ class OpportunityServiceTest {
     @MockBean
     OpportunityRepository repository;
 
+    Opportunity opportunity = new Opportunity("name", OpportunityCategory.ANIMALS, CreatorType.INDIVIDUAL,
+            "creatorId", "creatorName", "shortDescription", "detailedDescription",
+            LocalDate.now(), LocalTime.now(), LocalTime.now(), 1, "street", "houseNumber", "Vienna", 1000);
 
-//    @Test
-//    void saveOpportunityAlreadyExists() {
-//        Opportunity opportunity = new Opportunity("help", "description", LocalDate.now(), LocalTime.now(), LocalTime.now(), "category",
-//                "creator", "creatorName", "Vienna", 1);
-//        assertEquals(0, opportunity.getHashcode());
-//        when(repository.findOneByHashcode(opportunity.hashCode()))
-//                .thenReturn(Optional.of(opportunity));
-//
-//        opportunityService.save(opportunity);
-//        assertEquals(0, opportunity.getHashcode());
-//        verify(repository).findOneByHashcode(any(Integer.class));
-//        verifyNoMoreInteractions(repository);
-//    }
+    @Test
+    void saveOpportunityAlreadyExists() {
+        assertEquals(0, opportunity.getHashcode());
+        when(repository.findOneByHashcode(opportunity.hashCode()))
+                .thenReturn(Optional.of(opportunity));
+
+        opportunityService.save(opportunity);
+        assertEquals(0, opportunity.getHashcode());
+        verify(repository).findOneByHashcode(any(Integer.class));
+        verifyNoMoreInteractions(repository);
+    }
 
 
-//    @Test
-//    void saveOpportunityDoesNotExist() {
-//        Opportunity opportunity = new Opportunity("help", "description", LocalDate.now(), LocalTime.now(), LocalTime.now(), "category",
-//                "individual", "creatorName", "Vienna", 1);
-//        assertEquals(0, opportunity.getHashcode());
-//        when(repository.findOneByHashcode(opportunity.hashCode()))
-//                .thenReturn(Optional.empty());
-//        opportunityService.save(opportunity);
-//        verify(repository).findOneByHashcode(any(Integer.class));
-//        assertNotNull(opportunity.getHashcode());
-//        verify(repository).save(opportunity);
-//    }
+    @Test
+    void saveOpportunityDoesNotExist() {
+        assertEquals(0, opportunity.getHashcode());
+        when(repository.findOneByHashcode(opportunity.hashCode()))
+                .thenReturn(Optional.empty());
+        opportunityService.save(opportunity);
+        verify(repository).findOneByHashcode(any(Integer.class));
+        assertNotNull(opportunity.getHashcode());
+        verify(repository).save(opportunity);
+    }
 
-//    @Test
-//    void markAsDoneDoesNotFindEntry() {
-//        String id = "id";
-//        Optional<Opportunity> oExpected = Optional.empty();
-//        when(repository.findById(id))
-//                .thenReturn(oExpected);
-//        Optional<Opportunity> oResult = opportunityService.markAsDone(id);
-//        assertEquals(oExpected, oResult);
-//        verify(repository).findById(id);
-//    }
+    @Test
+    void markAsDoneDoesNotFindEntry() {
+        String id = "id";
+        Optional<Opportunity> oExpected = Optional.empty();
+        when(repository.findById(id))
+                .thenReturn(oExpected);
+        Optional<Opportunity> oResult = opportunityService.markAsDone(id);
+        assertEquals(oExpected, oResult);
+        verify(repository).findById(id);
+    }
 
-//    @Test
-//    void markAsDoneFindsEntry() {
-//        String id = "id";
-//        Optional<Opportunity> oExpected = Optional.of( new Opportunity("help", "description", LocalDate.now(), LocalTime.now(), LocalTime.now(), "category",
-//                "individual", "creatorName", "Vienna", 1));
-//        when(repository.findById(id))
-//                .thenReturn(oExpected);
-//        Optional<Opportunity> oResult = opportunityService.markAsDone(id);
-//        oExpected.get().setDone(true);
-//        assertEquals(oExpected, oResult);
-//        verify(repository).findById(id);
-//        verify(repository).save(oExpected.get());
-//    }
+    @Test
+    void markAsDoneFindsEntry() {
+        String id = "id";
+        Optional<Opportunity> oExpected = Optional.of(opportunity);
+        when(repository.findById(id)).thenReturn(oExpected);
+        Optional<Opportunity> oResult = opportunityService.markAsDone(id);
+        oExpected.get().setDone(true);
+        assertEquals(oExpected, oResult);
+        verify(repository).findById(id);
+        verify(repository).save(oExpected.get());
+    }
 }
 
--- src/test/java/com/project/apptruistic/logic/VolunteerServiceTest.java
+++ src/test/java/com/project/apptruistic/logic/VolunteerServiceTest.java
@@ -1,66 +1,69 @@
-//package com.project.apptruistic.logic;
-//
-//import com.project.apptruistic.persistence.domain.Volunteer;
-//import com.project.apptruistic.persistence.repository.VolunteerRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import java.time.LocalDate;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//import static org.mockito.Mockito.*;
-//import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
-//
-//@SpringBootTest(webEnvironment = NONE)
-//class VolunteerServiceTest {
-//
-//    @Autowired
-//    VolunteerService volunteerService;
-//
-//    @MockBean
-//    VolunteerRepository repository;
-//
-//    @MockBean
-//    PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    void findOneByEmail() {
-//        String email = "email";
-//        volunteerService.getVolunteerByEmail(email);
-//
-//        verify(repository).findOneByEmail(email);
-//    }
-//
-//    @Test
-//    void saveAlreadyExists() {
-//        Volunteer volunteer = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
-//        when(repository.findOneByEmail(volunteer.getEmail()))
-//                .thenReturn(Optional.of(volunteer));
-//
-//        volunteerService.save(volunteer);
-//
-//        verify(repository).findOneByEmail(volunteer.getEmail());
-//        verifyNoMoreInteractions(repository);
-//        verifyNoInteractions(passwordEncoder);
-//    }
-//
-//    @Test
-//    void saveVolunteerDoesNotExist() {
-//        Volunteer volunteer = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
-//        when(repository.findOneByEmail(volunteer.getEmail()))
-//                .thenReturn(Optional.empty());
-//        when(passwordEncoder.encode(volunteer.getPassword()))
-//                .thenReturn(volunteer.getPassword());
-//
-//        volunteerService.save(volunteer);
-//
-//        verify(repository).findOneByEmail(volunteer.getEmail());
-//        verify(passwordEncoder).encode(volunteer.getPassword());
-//        Volunteer expected = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
-//        verify(repository).save(expected);
-//    }
-//}
+package com.project.apptruistic.logic;
+
+import com.project.apptruistic.persistence.domain.Volunteer;
+import com.project.apptruistic.persistence.repository.VolunteerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class VolunteerServiceTest {
+    @MockBean
+    ApplicationRunner applicationRunner;
+
+    @Autowired
+    VolunteerService volunteerService;
+
+    @MockBean
+    VolunteerRepository repository;
+
+    @MockBean
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    void findOneByEmail() {
+        String email = "email";
+        volunteerService.getVolunteerByEmail(email);
+
+        verify(repository).findOneByEmail(email);
+    }
+
+    @Test
+    void saveAlreadyExists() {
+        Volunteer volunteer = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
+        when(repository.findOneByEmail(volunteer.getEmail()))
+                .thenReturn(Optional.of(volunteer));
+
+        volunteerService.save(volunteer);
+
+        verify(repository).findOneByEmail(volunteer.getEmail());
+        verifyNoMoreInteractions(repository);
+        verifyNoInteractions(passwordEncoder);
+    }
+
+    @Test
+    void saveVolunteerDoesNotExist() {
+        Volunteer volunteer = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
+        when(repository.findOneByEmail(volunteer.getEmail()))
+                .thenReturn(Optional.empty());
+        when(passwordEncoder.encode(volunteer.getPassword()))
+                .thenReturn(volunteer.getPassword());
+
+        volunteerService.save(volunteer);
+
+        verify(repository).findOneByEmail(volunteer.getEmail());
+        verify(passwordEncoder).encode(volunteer.getPassword());
+        Volunteer expected = new Volunteer("ciao", "ciao", LocalDate.now(), "gender", "ciao", "ciao", Set.of("categories"));
+        verify(repository).save(expected);
+    }
+}
