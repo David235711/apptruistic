81cba49306dd7748f204dcacedac16fce8065fe2
David235711 <info@davidwang.at>
2020-07-02 15:12:54 +0200

    MongoLocalTime

---
+++ src/main/java/com/project/apptruistic/logic/time/MongoLocalTime.java
@@ -0,0 +1,41 @@
+package com.project.apptruistic.logic.time;
+
+import java.time.LocalTime;
+
+public class MongoLocalTime implements Comparable<MongoLocalTime> {
+    private LocalTime localTime;
+
+    private MongoLocalTime(LocalTime localTime) {
+        this.localTime = localTime;
+    }
+
+    public static MongoLocalTime of(LocalTime localTime) {
+        return new MongoLocalTime(localTime);
+    }
+
+    public static MongoLocalTime of(int hour, int minute) {
+        return new MongoLocalTime(LocalTime.of(hour, minute));
+    }
+
+    public int getHour() {
+        return this.localTime.getHour();
+    }
+
+    public int getMinute() {
+        return this.localTime.getMinute();
+    }
+
+    public LocalTime toLocalTime() {
+        return this.localTime;
+    }
+
+    @Override
+    public int compareTo(MongoLocalTime other) {
+        return this.localTime.compareTo(other.localTime);
+    }
+
+    @Override
+    public String toString() {
+        return this.localTime.toString();
+    }
+}
