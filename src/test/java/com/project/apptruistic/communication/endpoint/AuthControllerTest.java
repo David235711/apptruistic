c139dcb693929c4cfe27f2d4aa851acf608e967f
GinaMiso <eugenia.buccellato@gmail.com>
2020-08-13 19:00:06 +0200

    test auth updated

--- src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
+++ src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
@@ -1,80 +1,49 @@
 package com.project.apptruistic.communication.endpoint;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.apptruistic.security.payload.request.LoginRequest;
-import org.junit.Before;
+import com.project.apptruistic.ApptruisticApplication;
+import com.project.apptruistic.logic.mail.MailServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.security.web.FilterChainProxy;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-//@WebAppConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {ApptruisticApplication.class})
 class AuthControllerTest {
 
-    @Autowired
-    private WebApplicationContext wac;
+    @MockBean
+    MailServiceImpl mail;
 
     @Autowired
-    private FilterChainProxy springSecurityFilterChain;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
-                .addFilter(springSecurityFilterChain).build();
-    }
-
-    private String saveToken(String email, String password) throws Exception {
-        LoginRequest loginRequest = new LoginRequest(email, password);
-        ResultActions result
-                = mockMvc.perform(post("/login")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(asJsonString(loginRequest))
-                .accept("application/json;charset=UTF-8"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType
-                        ("application/json;charset=UTF-8"));
-
-        String resultString = result.andReturn().getResponse().getContentAsString();
-
-        JacksonJsonParser jsonParser = new JacksonJsonParser();
-        return jsonParser.parseMap(resultString).get("access_token").toString();
-    }
-
-    public static String asJsonString(final Object obj) {
-        try {
-            final ObjectMapper mapper = new ObjectMapper();
-            final String jsonContent = mapper.writeValueAsString(obj);
-            System.out.println(jsonContent);
-            return jsonContent;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private MockMvc mvc;
 
     @Test
-    void volunteerAccessIndividual() throws Exception {
-        String accessToken = saveToken("test@volunteer", "00000000");
-        System.out.println(accessToken);
-        mockMvc.perform(get("/individualinformation/in1@com")
-                .header("Authorization", "Bearer " + accessToken))
-                .andExpect(status().isForbidden());
+    public void existentUserCanGetTokenAndAuthentication() throws Exception {
+        String email = "test@volunteer";
+        String password = "00000000";
+
+        String body = "{\"email\":\"" + email + "\", \"password\":\""
+                + password + "\"}";
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/login")
+                .content(body))
+                .andExpect(status().isOk()).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"access_token\": \"", "");
+        String token = response.replace("\"}", "");
+
+        mvc.perform(MockMvcRequestBuilders.get("/test")
+                .header("Authorization", "Bearer " + token))
+                .andExpect(status().isOk());
     }
 
     @Test
