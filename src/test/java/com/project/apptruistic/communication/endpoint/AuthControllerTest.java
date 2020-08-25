c7237ec065ee18774341dd1688384db24018715c
David235711 <info@davidwang.at>
2020-08-25 21:03:06 +0200

    Login and retrieve token unit test working

--- src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
+++ src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
@@ -1,49 +1,96 @@
 package com.project.apptruistic.communication.endpoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.apptruistic.ApptruisticApplication;
-import com.project.apptruistic.logic.mail.MailServiceImpl;
+import com.project.apptruistic.logic.CreatorType;
+import com.project.apptruistic.logic.OpportunityCategory;
+import com.project.apptruistic.logic.time.MongoLocalTime;
+import com.project.apptruistic.persistence.domain.Opportunity;
+import com.project.apptruistic.security.payload.request.LoginRequest;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {ApptruisticApplication.class})
+@WebAppConfiguration
+@SpringBootTest(classes = {ApptruisticApplication.class})
 class AuthControllerTest {
 
-    @MockBean
-    MailServiceImpl mail;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private MockMvc mvc;
 
     @Autowired
-    private MockMvc mvc;
+    private WebApplicationContext webApplicationContext;
+
+    private LoginRequest loginRequest;
+
+// This does not work as expected
+//    @Before
+//    public void setup() {
+//        this.mvc = webAppContextSetup(this.webApplicationContext).build();
+//    }
 
     @Test
     public void existentUserCanGetTokenAndAuthentication() throws Exception {
+        this.mvc = webAppContextSetup(this.webApplicationContext).build();
         String email = "test@volunteer";
         String password = "00000000";
+        loginRequest = new LoginRequest(email, password);
+        String body = objectMapper.writeValueAsString(loginRequest);
 
-        String body = "{\"email\":\"" + email + "\", \"password\":\""
-                + password + "\"}";
-
-        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/login")
+        ResultActions result = mvc.perform(MockMvcRequestBuilders.post("/login")
+                .contentType("application/json")
                 .content(body))
-                .andExpect(status().isOk()).andReturn();
-
-        String response = result.getResponse().getContentAsString();
-        response = response.replace("{\"access_token\": \"", "");
-        String token = response.replace("\"}", "");
-
-        mvc.perform(MockMvcRequestBuilders.get("/test")
-                .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk());
+        String response = result.andReturn().getResponse().getContentAsString();
+        System.out.println("Response: " + response);
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        String role = jsonParser.parseMap(response).get("roles").toString();
+        String token = jsonParser.parseMap(response).get("accessToken").toString();
+        System.out.println("Role: " + role);
+        System.out.println("Token: " + token);
+/* Server response 400, bad request
+        Opportunity opportunity = new Opportunity(
+                "name",
+                OpportunityCategory.ANIMALS,
+                CreatorType.INDIVIDUAL,
+                "creatorId",
+                "creatorName",
+                "shortDescription",
+                "detailedDescription",
+                LocalDate.of(2020, 6, 22),
+                MongoLocalTime.of(LocalTime.now()),
+                MongoLocalTime.of(LocalTime.now()),
+                1,
+                "street",
+                "houseNumber",
+                "Vienna",
+                1000);
+        String opportunityBody = objectMapper.writeValueAsString(opportunity);
+        mvc.perform(MockMvcRequestBuilders.post("/opportunities")
+                .header("Authorization", "Bearer " + token)
+                .contentType("application/json")
+                .content(opportunityBody))
+                .andExpect(status().isForbidden());*/
     }
 
     @Test
