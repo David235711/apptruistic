81427efc58b1b721b7866fd4dde69db6a5cc3b0c
David235711 <info@davidwang.at>
2020-09-24 14:17:52 +0200

    new test without token fails

--- src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
+++ src/test/java/com/project/apptruistic/communication/endpoint/AuthControllerTest.java
@@ -2,29 +2,21 @@ package com.project.apptruistic.communication.endpoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.apptruistic.ApptruisticApplication;
-import com.project.apptruistic.logic.CreatorType;
-import com.project.apptruistic.logic.OpportunityCategory;
-import com.project.apptruistic.logic.time.MongoLocalTime;
-import com.project.apptruistic.persistence.domain.Opportunity;
 import com.project.apptruistic.security.payload.request.LoginRequest;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -43,17 +35,17 @@ class AuthControllerTest {
 
     private LoginRequest loginRequest;
 
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+    private static final String EMAIL = "test@volunteer";
+    public static final String PASSWORD = "00000000";
+
 // This does not work as expected
 //    @Before
 //    public void setup() {
 //        this.mvc = webAppContextSetup(this.webApplicationContext).build();
 //    }
 
-    @Test
-    public void existentUserCanGetTokenAndAuthentication() throws Exception {
-        this.mvc = webAppContextSetup(this.webApplicationContext).build();
-        String email = "test@volunteer";
-        String password = "00000000";
+    private String obtainAccessToken(String email, String password) throws Exception {
         loginRequest = new LoginRequest(email, password);
         String body = objectMapper.writeValueAsString(loginRequest);
 
@@ -62,14 +54,24 @@ class AuthControllerTest {
                 .content(body))
                 .andExpect(status().isOk());
         String response = result.andReturn().getResponse().getContentAsString();
-        System.out.println("Response: " + response);
         JacksonJsonParser jsonParser = new JacksonJsonParser();
-        String role = jsonParser.parseMap(response).get("roles").toString();
-        String token = jsonParser.parseMap(response).get("accessToken").toString();
-        System.out.println("Role: " + role);
+//        String role = jsonParser.parseMap(response).get("roles").toString();
+        return jsonParser.parseMap(response).get("accessToken").toString();
+    }
+
+    @Test
+    public void givenToken_whenGetSecuredRequest_thenOk() throws Exception {
+        this.mvc = webAppContextSetup(this.webApplicationContext).build();
+        String token = obtainAccessToken(EMAIL, PASSWORD);
         System.out.println("Token: " + token);
-/* Server response 400, bad request
-        Opportunity opportunity = new Opportunity(
+
+        mvc.perform(MockMvcRequestBuilders.get("/opportunities/availables")
+                .header("Authorization", "Bearer " + token)
+                .accept(CONTENT_TYPE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE));
+// Server response 400, bad request
+       /* Opportunity opportunity = new Opportunity(
                 "name",
                 OpportunityCategory.ANIMALS,
                 CreatorType.INDIVIDUAL,
@@ -91,6 +93,16 @@ class AuthControllerTest {
                 .contentType("application/json")
                 .content(opportunityBody))
                 .andExpect(status().isForbidden());*/
+    }
+
+    @Test
+    public void givenNoToken_whenGetSecuredRequest_thenUnauthorized() throws Exception {
+        this.mvc = webAppContextSetup(this.webApplicationContext).build();
+                    mvc.perform(MockMvcRequestBuilders.get("/opportunities/availables")
+                    .header("Authorization", "Bearer " + "xx")
+                    .accept(CONTENT_TYPE))
+                    .andExpect(status().is5xxServerError());
+
     }
 
     @Test
